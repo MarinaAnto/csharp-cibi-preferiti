144234a2be9547757be3a21d82504e88826c6d0f
MarinaAnto <antonellaarmas160@gmail.com>
2022-04-03 09:12:53 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] cibi = { "carbonara", "nutella", "cappuccino", "pizza", "quacker" };
+int pos = -1;
+foreach (string s in cibi){
+    pos++;
+    Console.WriteLine(pos + " : " + s);
+}
+Console.WriteLine("La lunghezza è: " + cibi.Length);
+Console.WriteLine("Il mio cibo top è: " + cibi[0]);
+Console.WriteLine("Il mio cibo preferito ma non troppo è: " + cibi[cibi.Length - 1]);
